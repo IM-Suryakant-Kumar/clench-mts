a1f3208e2d271b0e94ab49d7d8e79102d3a4e65b
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-03-19 14:07:17 +0530

    Server setup

---
+++ server/src/app.ts
@@ -0,0 +1,38 @@
+import "express-async-errors";
+import { config } from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandlerMiddleware, notFoundMiddleware } from "./middlewares";
+
+const app = express();
+config();
+
+// constant
+const CLIENT_URL: string = process.env.CLIENT_URL;
+const PORT: number = Number(process.env.PORT) || 4000
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+// routes
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+const start = async () => {
+	try {
+		app.listen(PORT, () =>
+			console.log(`Server is listening on port ${PORT}...`)
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
+start();
