d64cd80d426fdf2257f3d93db6977e4284d0bc37
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-03-19 15:00:04 +0530

    feat: updated NotFound page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,7 +1,11 @@
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  );
-}
+	return (
+		<div className="min-h-screen flex justify-center items-center">
+			<h2 className="text-red-500 text-xl font-semibold text-center md:text-3xl">
+				404 - PAGE NOT FOUND!
+			</h2>
+		</div>
+	);
+};
 
 export default NotFound;
